9af198a96123ff613fea5e3f9eac28f99eefe0c9
Allysht <81968946+Allysht@users.noreply.github.com>
2022-03-07 16:43:20 +0200

    1

---
+++ server.js
@@ -0,0 +1,18 @@
+const PORT = 8000
+const express = require('express')
+const path = require('path')
+
+const app = express();
+app.set('view engine', 'ejs');
+
+const staticPath = path.join(__dirname, 'public')
+
+app.use(express.static(staticPath))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(PORT, (req, res) => {
+    console.log(`app running on port: ${PORT}`)
+})
